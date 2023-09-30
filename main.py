f81613fddadab00abdd854933d7f7f9e49cb6512
yosef <yosefsusanto7@gmail.com>
2023-09-30 23:40:16 +0700

    baruuu

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Load data from Excel file
-excel_file_path = "data_dokter_1.xlsx"  # Ganti dengan path file Excel Anda
+excel_file_path = "data_dokter.xlsx"
 df = pd.read_excel(excel_file_path)
 
 # Set page title
@@ -11,28 +11,16 @@ st.set_page_config(page_title="Admin Panel")
 # Page header
 st.header("Admin Panel")
 
-# Sidebar untuk konfigurasi informasi
-st.sidebar.subheader("Pengaturan")
-selected_day = st.sidebar.selectbox("Pilih Hari", df["Hari"].unique())
-show_all_doctors = st.sidebar.checkbox("Tampilkan Semua Dokter")
+# Sidebar untuk pengaturan data yang akan ditampilkan
+st.sidebar.subheader("Pengaturan Tampilan")
+show_doctors = st.sidebar.checkbox("Tampilkan Dokter")
+show_schedule = st.sidebar.checkbox("Tampilkan Jadwal Praktek")
 
-# Tombol untuk menampilkan data di layar utama
-if st.sidebar.button("Tampilkan di Layar"):
-    # Menampilkan informasi dokter yang dipilih atau semua dokter
-    if show_all_doctors:
-        filtered_df = df[df["Hari"] == selected_day]
-    else:
-        filtered_df = df[(df["Hari"] == selected_day)]
+# Menampilkan data berdasarkan pengaturan yang dipilih
+if show_doctors:
+    st.subheader("Informasi Dokter")
+    st.table(df["Nama Dokter"])  # Menggantilah dengan nama kolom yang sesuai dengan data dokter Anda
 
-    # Menghilangkan kolom "Hari" dari tabel
-    filtered_df = filtered_df.drop(columns=["Hari"])
-    
-    # Urutkan berdasarkan kolom jam praktek (jika ada)
-    if "Jam" in filtered_df.columns:
-        filtered_df = filtered_df.sort_values(by=["Jam"])  # Ganti "Jam" dengan nama kolom jam praktek
-        
-    # Tampilkan nama hari di atas kanan tabel
-    st.markdown(f"**Hari: {selected_day}**")
-    
-    # Tampilkan informasi dokter dalam bentuk tabel tanpa nomor urut
-    st.markdown(filtered_df.to_markdown(index=False), unsafe_allow_html=True)
+if show_schedule:
+    st.subheader("Jadwal Praktek Dokter")
+    st.table(df[["Nama Dokter", "Hari", "Jam"]])  # Menggantilah dengan nama kolom yang sesuai dengan jadwal praktek Anda
