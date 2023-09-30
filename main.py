44fa3640e3fcc6e160fa9984361eb1a076578697
yosef <yosefsusanto7@gmail.com>
2023-09-30 17:43:10 +0700

    BARUu

--- main.py
+++ main.py
@@ -2,32 +2,37 @@ import streamlit as st
 import pandas as pd
 
 # Load data from Excel file
-excel_file_path = "data_dokter.xlsx"  # Ganti dengan path file Excel Anda
+excel_file_path = "data_dokter_1.xlsx"  # Ganti dengan path file Excel Anda
 df = pd.read_excel(excel_file_path)
 
 # Set page title
-st.set_page_config(page_title="Display Informasi Praktek Dokter")
+st.set_page_config(page_title="Admin Panel")
 
 # Page header
-st.header("Informasi Praktek Dokter")
+st.header("Admin Panel")
 
-# Sidebar untuk filter dokter
-st.sidebar.subheader("Filter Dokter")
+# Sidebar untuk konfigurasi informasi
+st.sidebar.subheader("Pengaturan")
 selected_day = st.sidebar.selectbox("Pilih Hari", df["Hari"].unique())
+show_all_doctors = st.sidebar.checkbox("Tampilkan Semua Dokter")
 
-# Filter data berdasarkan hari yang dipilih
-filtered_df = df[df["Hari"] == selected_day]
+# Tombol untuk menampilkan data di layar utama
+if st.sidebar.button("Tampilkan di Layar"):
+    # Menampilkan informasi dokter yang dipilih atau semua dokter
+    if show_all_doctors:
+        filtered_df = df[df["Hari"] == selected_day]
+    else:
+        filtered_df = df[(df["Hari"] == selected_day)]
 
-# Tampilkan data dokter
-st.subheader("Daftar Dokter")
-st.table(filtered_df)
-
-# Tampilkan kuota dan status dokter
-st.subheader("Status Dokter")
-for index, row in filtered_df.iterrows():
-    st.write(f"Dokter: {row['Dokter']}")
-    st.write(f"Kuota: {row['Kuota']}")
-    st.write(f"Status: {'Tutup' if row['Status'] == 'Tutup' else 'Buka'}")
-
-# Informasi tambahan
-st.info("Data di atas adalah informasi praktek dokter berdasarkan hari yang dipilih.")
+    # Menghilangkan kolom "Hari" dari tabel
+    filtered_df = filtered_df.drop(columns=["Hari"])
+    
+    # Urutkan berdasarkan kolom jam praktek (jika ada)
+    if "Jam" in filtered_df.columns:
+        filtered_df = filtered_df.sort_values(by=["Jam"])  # Ganti "Jam" dengan nama kolom jam praktek
+        
+    # Tampilkan nama hari di atas kanan tabel
+    st.markdown(f"**Hari: {selected_day}**")
+    
+    # Tampilkan informasi dokter dalam bentuk tabel tanpa nomor urut
+    st.markdown(filtered_df.to_markdown(index=False), unsafe_allow_html=True)
